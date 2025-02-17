ad214b99a248d7ff64189fc3dd638490a981b0b2
BertusKristof <kristofbertus@gmail.com>
2025-02-17 16:18:41 +0100

    BertusKristof20250217

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
-import { ProductsComponent } from './products/products.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { ProductsComponent } from './products/products.component';
+import { ProductService } from '../services/product.service';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: false,
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  template:`
+      <button (click)="navigateTo('parent')">Parent</button>
+      <button (click)="navigateTo('products')">Products</button>
+  `,
+  // templateUrl: './navigation.component.html',
+  // styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
